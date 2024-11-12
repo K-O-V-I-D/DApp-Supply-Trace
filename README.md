dc597652590d8f4996b25aad2e3e2718541ae0f0
KOVID TIWARI <63060096+K-O-V-I-D@users.noreply.github.com>
2024-11-12 22:24:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DApp-Supply-Trace
+## Supply-Chain-Management-System
+- The repository aims to provide a Supply Chain Management system Dapp designed for consumers and producers. It likely includes smart contracts, a user interface, and other components for tracking and managing the supply chain process.
+  
+#### Features :
+- Producer can add the products in the bucket-list.
+- Consumer can buy the products from the bucket-list by using productID.
+- Producer can also update the price of the products and handle the delivery status of the products.
+- Consumer can track the Products and get the delivery status of the products.
+
+#### VSCode Setup:-
+![Screenshot from 2022-11-07 23-30-11](https://user-images.githubusercontent.com/76531339/200382102-eb9e0532-b431-4aa2-a734-84cfe46aa33f.png)
+
+#### Producer Page
+![Screenshot from 2022-11-07 23-14-44](https://user-images.githubusercontent.com/76531339/200379085-f273bd17-4cf9-431a-bdfb-d8b4f22ee043.png)
+
+![Screenshot from 2022-11-07 23-15-08](https://user-images.githubusercontent.com/76531339/200379154-ca904cff-49fc-4d81-87f7-0240a98f1b7f.png)
+
+#### Consumer Page
+![Screenshot from 2022-11-07 23-15-37](https://user-images.githubusercontent.com/76531339/200379191-7685b76f-91e5-4c5d-94d0-35bba07955fa.png)
+
+
---
+++ consumer.js
@@ -0,0 +1,590 @@
+var abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_pname",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_quantity",
+				"type": "uint256"
+			}
+		],
+		"name": "addNewProductsInList",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_oid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_status",
+				"type": "string"
+			}
+		],
+		"name": "giveOrderItsStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_pid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_newPrice",
+				"type": "uint256"
+			}
+		],
+		"name": "newPrice",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_pid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_quantity",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_cname",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_daddress",
+				"type": "string"
+			}
+		],
+		"name": "placeAnOrder",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "registerNewProducer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			}
+		],
+		"name": "getMyTotalOrder",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_oid",
+				"type": "uint256"
+			}
+		],
+		"name": "getOrderByIdConsumer",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_oid",
+				"type": "uint256"
+			}
+		],
+		"name": "getOrderByIdProducer",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_pid",
+				"type": "uint256"
+			}
+		],
+		"name": "getProductbyId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_pid",
+				"type": "uint256"
+			}
+		],
+		"name": "getProductById",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			}
+		],
+		"name": "getTotalOrder",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTotalOrder",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			}
+		],
+		"name": "getTotalProduct",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTotalProduct",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_addr",
+				"type": "address"
+			}
+		],
+		"name": "isProducerRegistered",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "orders",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "product_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "quantity",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "customer_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "status",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "delivery_address",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "customer_address",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "producers",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "products",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "quantity",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "product_name",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "producer_address",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalOrder",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalProduct",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+var address = "0xCc10D8a7687051Ec063347555d7EA2Ee5722F26a";
+
+web3 = new Web3(web3.currentProvider);
+var contract = new web3.eth.Contract(abi, address);
+console.log("blockchain connected")
+
+$(document).ready(function () {
+
+contract.methods.getTotalProduct().call().then(function (totalProduct) {
+    console.log("totalProduct : " + totalProduct);
+    $("#_products_table").html(totalProduct);
+
+    for (var i = 1; i <= totalProduct; i++) {
+        contract.methods.getProductById(i).call().then(function (productDetails) {
+            console.log(productDetails);
+            var row = "<tr><th>" + productDetails[0] + "</th><td>" + productDetails[3] + "</td><td>" + productDetails[1] + "</td><td>" + productDetails[2] + "</td><td><button type=\"button\" class=\"btn btn-secondary btn-sm\" onclick=\"productOrderClick(" + productDetails[0] + ")\">Order</button></td></tr>";
+            $("#_product_table").find('tbody').append(row);
+        });
+    }
+});
+
+
+web3.eth.getAccounts().then(function (accounts) {
+    var account = accounts[0];
+
+
+    contract.methods.getTotalOrder(account).call().then(function (totalOrder) {
+        console.log("totalOrder : " + totalOrder);
+        $("#_track").html(totalOrder);
+    });
+
+    contract.methods.getTotalOrder().call().then(function (totalOrder) {
+        console.log("totalOrder (global) : " + totalOrder);
+
+        for (var index = 1; index <= totalOrder; index++) {
+            contract.methods.getOrderByIdConsumer(account, index).call().then(function (orderDetails) {
+                console.log(orderDetails);
+                if (orderDetails[3]) {
+                    var row = "<tr><th scope=\"row\">" + orderDetails[0] + "</th><td>" + orderDetails[2] + "</td><td>" + orderDetails[1] + "</td></tr>";
+                    $("#_order_table").find('tbody').append(row);
+                }
+            });
+        }
+    });
+
+
+
+});
+
+
+$("#_orderbtn").click(function () {
+    web3.eth.getAccounts().then(function (accounts) {
+        var account = accounts[0];
+        var pid = $("#_pid").val();
+        var quantity = $("#_quantity").val();
+        var cname = $("#_cname").val();
+        var caddress = $("#_caddress").val();
+        console.log("place order : " + pid + quantity + cname + caddress);
+
+        return contract.methods.placeAnOrder(pid,quantity,cname, caddress).send({ from: account });
+    }).then(function (trx) {
+        console.log(trx);
+        if (trx.status) {
+            alert("Order is placed!");
+            $("#_pid").val("");
+            $("#_quantity").val("");
+            $("#_cname").val("");
+            $("#_caddress").val("");
+            location.reload();
+        }
+
+    });
+});
+
+
+});
+
+function productOrderClick(productId) {
+console.log("order click : " + productId);
+$("#_pid").val(productId);
+}
